42557c1086816e37c2d5f7a315d62eefad20fa27
D.S. Ljungmark <spider@skuggor.se>
2016-05-08 01:45:50 +0200

    Move the client code into the root
    This concludes the split. Most

--- setup.py
+++ setup.py
@@ -3,54 +3,25 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+
 README = open(os.path.join(here, "README.md")).read()
 CHANGES = open(os.path.join(here, "CHANGES.txt")).read()
 
-requires = [
-    "pyramid",
-    "SQLAlchemy",
-    "transaction",
-    "pyramid_tm",
-    # "pyramid_debugtoolbar",
-    "zope.sqlalchemy",
-    "waitress",
-    "cryptography>=0.5.dev1",
-    "pyOpenSSL>=0.14",
-    "flup<=1.0.4.dev",
-    "python-dateutil",
-    ]
 
-deplinks = [
-    "http://hg.saddi.com/flup-py3.0/archive/cc23b715b120.tar.gz" +
-    "#egg=flup-1.0.4.dev",
-    ]
-
-setup(name="caramel",
-      version="1.0",
-      description="caramel",
-      long_description=README + "\n\n" + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-          ],
-      author="D.S. Ljungmark",
-      author_email="spider@modio.se",
-      url="https://github.com/MyTemp/caramel",
-      keywords="web wsgi bfg pylons pyramid certificates x509 ca cert",
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite="tests",
-      install_requires=requires,
-      dependency_links=deplinks,
-      entry_points="""\
-      [paste.app_factory]
-      main = caramel:main
-      [console_scripts]
-      caramel_initialize_db = caramel.scripts.initializedb:main
-      caramel_tool = caramel.scripts.tool:main
-      caramel_ca = caramel.scripts.generate_ca:main
-      """,
-      )
+setup(
+    name="caramel-client",
+    version="1.0",
+    description="caramel-client",
+    packages=find_packages(),
+    scripts=['caramel-client'],
+    long_description=README + "\n\n" + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author="D.S. Ljungmark",
+    author_email="spider@modio.se",
+    url="https://github.com/MyTemp/caramel-client",
+    keywords="caramel ssl tls certificates x509 ca cert",
+    include_package_data=True,
+    zip_safe=True,
+)
